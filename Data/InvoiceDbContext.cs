c150a0ddd05aee574aaa34b4d2049a855fcb2112
YU-RU <sandyandmi@gmail.com>
2021-01-13 10:28:11 +0800

    chore: DbContext Settings

---
+++ Data/InvoiceDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Invoice.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Invoice.Data
+{
+    public class InvoiceDbContext : DbContext
+    {
+        public InvoiceDbContext(DbContextOptions<InvoiceDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Account_Role> Account_Role { get; set; }
+    }
+}
--- Models/Account_Role.cs
+++ Models/Account_Role.cs
@@ -8,19 +8,9 @@ using System.Web;
 
 namespace Invoice.Models
 {
-    public enum Identity
-    {
-        [Description("管理者")]
-        Admin = 0,
-
-        [Description("註冊會員")]
-        Member = 1,
-    }
-
     [Table("Account_Role")]
     public class Account_Role
     {
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
         public Account_Role()
         {
             Account = new HashSet<Account>();
@@ -32,7 +22,6 @@ namespace Invoice.Models
         //Admin, Member, Organization
         public string Role_Name { get; set; }
 
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Account> Account { get; set; }
     }
 }
